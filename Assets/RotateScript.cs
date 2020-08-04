e7cc6575668fc44324574b901172db4f30329c60
MirOoll <47189644+MirOoll@users.noreply.github.com>
2020-08-04 19:22:12 +0300

    DemoVersion

--- Assets/RotateScript.cs
+++ Assets/RotateScript.cs
@@ -11,11 +11,23 @@ public class RotateScript : MonoBehaviour
     {
         
     }
+
+    //void Turning()
+    //{
+    //    RotateX = CrossPlatformInputManager.GetAxis("Mouse X");
+    //    RotateY = CrossPlatformInputManager.GetAxis("Mouse Y");
+    //    Vector3 relativePos = new Vector3(RotateX, 0, RotateY);
+    //    newPos += relativePos;
+    //    newPos = newPos.normalized * Time.deltaTime;
+    //    Quaternion rotation = Quaternion.LookRotation(newPos);
+    //    playerRigidbody.MoveRotation(rotation);
+    //}
+
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed);
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0).normalized * Time.deltaTime * speed);
         }
 
     }
